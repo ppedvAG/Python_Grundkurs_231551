4eea2d56c537d755dad709b878e0b5e14e1b18ff
ppedvLukasK <lukask@ppedv.de>
2023-07-20 15:49:43 +0200

    M001

--- M001.py
+++ M001.py
@@ -97,15 +97,30 @@ wort1 *= 5
 # Übung1
 # Lege drei numerische Variablen an, addiere sie zusammen und schreibe das Ergebnis in eine neue Variable
 # Potenziere danach die Variable mit sich selbst und schreibe das Ergebnis erneut in eine Variable
+z1 = 2
+z2 = 4
+z3 = 7
+ergebnis = z1 + z2 + z3
+potenz = ergebnis ** ergebnis
 
 # Übung2
 # Nimm die potenzierte Zahl aus Übung1 und stelle fest ob diese Restlos durch 2 teilbar (gerade) ist
+mod = potenz % 2
 
 # Übung3
 # Lege zwei Variablen an: Vorname befüllt mit Max und Nachname befüllt mit Mustermann
 # Verbinde diese zwei Variablen und zähle danach die Buchstaben 'M' und 'm'
 # Das Ergebnis soll 3 sein
+vorname = "Max"
+nachname = "Mustermann"
+gesamt = vorname + nachname
+print(gesamt.lower().count("m"))
 
 # Übung4
 # Schreibe deinen Vornamen ohne Großbuchstaben in eine Variable
 # Verwende danach diese Variable, und gib diese mit dem ersten Buchstaben groß geschrieben aus
+name = 'lukas'
+print(name[0].upper() + name[1:])
+print(name[0].upper() + name[1:len(name)])
+print(name.title())
+print(name.capitalize())

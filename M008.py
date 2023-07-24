abe4f93e45c0aa5e1ec966de3aeddb04b1703391
ppedvLukasK <lukask@ppedv.de>
2023-07-24 16:03:55 +0200

    M008

--- M008.py
+++ M008.py
@@ -60,6 +60,14 @@ if os.path.exists("Name"):
 # Wenn der User keine valide Möglichkeit eingibt, soll die Eingabe wiederholt werden
 # Danach soll einfach das File geöffnet werden
 # Bonus: Frage den Benutzer nach dem File, welches geöffnet werden soll
+def auswahl():
+	while True:  # Endlosschleife bis der User etwas korrektes eingibt
+		# if choice == "w" or choice == "r" or choice == "a":
+		choice = input("Gib w, r oder a ein: ")
+		if choice in ["w", "r", "a"]:  # in statt if/or
+			break
+	print("Valide Eingabe, File wird geöffnet")
+	return open("Test.txt", choice)
 
 # Übung 2:
 # Erstelle ein Programm, das zwei Integer oder Floats abfragt
@@ -68,3 +76,37 @@ if os.path.exists("Name"):
 # Bei Ungültiger Eingabe soll der Benutzer erneut nach seiner Entscheidung gefragt werden.
 # Lasse das Ergebnis inklusive der Berechnung in der Konsole ausgeben
 # Frage nach Ende der Operation ob der Benutzer eine neue Berechnung durchführen will
+def rechner():
+	while True:
+		while True:
+			zahl1 = input("Gib die erste Zahl ein: ")
+			if zahl1.isnumeric():
+				zahl1 = int(zahl1)
+				break
+
+		while True:
+			zahl2 = input("Gib die zweite Zahl ein: ")
+			if zahl2.isnumeric():
+				zahl2 = int(zahl2)
+				break
+
+		while True:
+			operation = input("Gib eine Rechenoperation ein (+, -, *, /): ")
+			if operation in ["+", "-", "*", "/"]:
+				break
+
+		if operation == "+":
+			print(f"{zahl1} + {zahl2} = {zahl1 + zahl2}")
+		if operation == "-":
+			print(f"{zahl1} - {zahl2} = {zahl1 - zahl2}")
+		if operation == "*":
+			print(f"{zahl1} * {zahl2} = {zahl1 * zahl2}")
+		if operation == "/":
+			print(f"{zahl1} / {zahl2} = {zahl1 / zahl2}")
+
+		wiederholung = input("Wiederholen? (Y)")
+		if wiederholung != "Y":
+			break
+
+if __name__ == "__main__":
+	rechner()
